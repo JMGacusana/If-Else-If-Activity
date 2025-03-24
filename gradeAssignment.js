9bc291c4babfca55691587f0180ef01a5e67c797
JOHN MARK GACUSANA <gacusanajohnmark1986@gmail.com>
2025-03-24 14:53:14 +0800

    Update

--- gradeAssignment.js
+++ gradeAssignment.js
@@ -3,7 +3,7 @@ let yourGrade = parseInt(prompt("Enter your grade: "));
 
 // If Else If Statement
 if(yourGrade >= 90){                           // this will execute the grade 90 and above
-   console.log("Exellent!");
+   console.log("Excellent!");
 }else if (yourGrade >= 80 && yourGrade <= 89){ // this will execute the grade from 80-89
    console.log("Good!");
 }else if (yourGrade >= 70 && yourGrade <= 79){ // this will execute the grade from 70-79
